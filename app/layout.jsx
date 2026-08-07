4eabb37986041e4ab0c313108a3d50bfa0dd04c6
masum-k <maasumkamal@gmail.com>
2026-08-07 23:13:37 +0600

    created file structure

---
+++ app/layout.jsx
@@ -0,0 +1,32 @@
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+import { site } from "@/lib/data";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"]
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"]
+});
+
+export const metadata = {
+  title: site.title,
+  description: site.description,
+  keywords: site.keywords
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
